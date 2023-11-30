bd7d4c7c044a8db915da3e84899a84c79bca3987
Michael Goldsmith <Michael.Goldsmith@datalex.com>
2023-11-30 04:18:02 +0000

    fixed compile issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,11 @@ import { DarkModeProvider, IsDarkModeContext } from './dark_mode_context'
 import { Inter } from 'next/font/google'
 import { useContext } from 'react'
 import { Props } from './props'
+import RootChild from './layout_child'
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Props) : React.ReactNode {
   return (
     <DarkModeProvider>
        <RootChild>
@@ -18,29 +17,3 @@ export default function RootLayout({
     </DarkModeProvider>
   )
 }
-
-
-const inter = Inter({ subsets: ['latin'] })
-
-export function RootChild(
-    props: Props
-) {
-    const darkModeContext = useContext(IsDarkModeContext)
-    var darkMode = darkModeContext.enabled
-    const isDarkMode = darkMode ? "dark" : ""
-    const htmlClass = isDarkMode + " scroll-smooth"
-    const bodyClassName = 'bg-neutral-100 dark:bg-neutral-900 ' + inter.className
-  return (
-
-        <html lang="th" className={htmlClass}>
-  
-          <body className={bodyClassName}>
-            <div id="home" />
-            <div className='bg-neutral-100 dark:bg-neutral-900'>
-              {props.children}
-            </div>
-          </body>
-      </html>
-  
-  );
-  }
---
+++ src/app/layout_child.tsx
@@ -0,0 +1,29 @@
+import { Inter } from "next/font/google"
+import { Props } from "./props"
+import { useContext } from "react"
+import { IsDarkModeContext } from "./dark_mode_context"
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootChild({children}: Props
+) {
+    const darkModeContext = useContext(IsDarkModeContext)
+    var darkMode = darkModeContext.enabled
+    const isDarkMode = darkMode ? "dark" : ""
+    const htmlClass = isDarkMode + " scroll-smooth"
+    const bodyClassName = 'bg-neutral-100 dark:bg-neutral-900 ' + inter.className
+  return (
+
+        <html lang="th" className={htmlClass}>
+  
+          <body className={bodyClassName}>
+            <div id="home" />
+            <div className='bg-neutral-100 dark:bg-neutral-900'>
+              {children}
+            </div>
+          </body>
+      </html>
+  
+  );
+  }
